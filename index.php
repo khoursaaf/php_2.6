46c29eab4f9e6b68d9600e47eaeebca141795bbc
khoursaaf <khoursa@gmail.com>
2018-11-20 16:23:32 +0100

    correction

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-      <p>Traduire ce code avec des if et des else :</p>
-      <p><code>echo ($age >= 18) ? 'Tu es majeur' : 'Tu n\'es pas majeur';</code></p>
-      <p>
-        <?php
-        $age = 13;
-        if ($age >= 18) {
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>Traduire ce code avec des if et des else :</p>
+    <p><code>echo ($age >= 18) ? 'Tu es majeur' : 'Tu n\'es pas majeur';</code></p>
+    <p>
+      <?php
+      $age = 13;
+      if ($age >= 18) {
         echo "Tu es majeur";
-        } else {
-          echo "Tu n'es pas majeur";
-        }
-         ?>
-      </p>
-    </body>
+      } else {
+        echo "Tu n'es pas majeur";
+      }
+      ?>
+    </p>
+  </body>
 </html>
